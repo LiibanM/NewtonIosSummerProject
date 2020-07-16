f8bd1ddec2aab7c5c49a0e473adc4077eaa7df7c
LiibanMukhtar <liiban.mukhtar@and.digital>
2020-07-16 22:21:36 +0100

    Initial Commit

---
+++ IosSummerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosSummerProject
+//
+//  Created by Liiban Mukhtar on 16/07/2020.
+//  Copyright © 2020 Liiban Mukhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
