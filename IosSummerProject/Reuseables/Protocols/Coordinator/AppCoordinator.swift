ef43cdc70ec5766305eb910b64f4d4bb00441635
akashmair <akash_mair@hotmail.co.uk>
2020-07-18 13:54:49 +0100

    feat(AppCoordinator): init the class and included an empty start()

--- IosSummerProject/Reuseables/Protocols/Coordinator/AppCoordinator.swift
+++ IosSummerProject/Reuseables/Protocols/Coordinator/AppCoordinator.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+    
+    override init(navigationController: UINavigationController) {
+        super.init(navigationController: navigationController)
+    }
+    
+    override func start() {
+        showLogin()
+    }
+    
+    func showLogin() {
+        
+    }
+}
