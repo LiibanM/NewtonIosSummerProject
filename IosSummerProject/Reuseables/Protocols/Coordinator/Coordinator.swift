6c71b16cfb530716901faa38bc027af42e309be2
akashmair <akash_mair@hotmail.co.uk>
2020-07-18 13:24:36 +0100

    feat(AppCoordinator): Created the Coordinator base class

--- IosSummerProject/Reuseables/Protocols/Coordinator/Coordinator.swift
+++ IosSummerProject/Reuseables/Protocols/Coordinator/Coordinator.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import UIKit
+
+protocol CoordinatorProtocol: class {
+    func start()
+    
+    var navigationController: UINavigationController { get set }
+}
+
+class Coordinator: CoordinatorProtocol {
+    
+    private(set) var childCoordinators: [Coordinator] = []
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        preconditionFailure("This method needs to be overriden by a concrete subclass.")
+    }
+    
+    func addChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+    
+    func removeChildCoordinator(_ coordinator: Coordinator) {
+        if let index = childCoordinators.firstIndex(of: coordinator) {
+            childCoordinators.remove(at: index)
+        } else {
+            print("Couldn't remove coordinator: \(coordinator). It's not a child coordinator.")
+        }
+    }
+    
+    func removeAllChildCoordinatorsWith<T>(type: T.Type) {
+        childCoordinators = childCoordinators.filter { $0 is T  == false }
+    }
+    
+    func removeAllChildCoordinators() {
+        childCoordinators.removeAll()
+    }
+    
+    func removeLastChildCoordinators() {
+        if let lastCoordinator = childCoordinators.last {
+            removeChildCoordinator(lastCoordinator)
+        }
+    }
+}
+extension Coordinator: Equatable {
+    static func == (lhs: Coordinator, rhs: Coordinator) -> Bool {
+        return lhs === rhs
+    }
+}
