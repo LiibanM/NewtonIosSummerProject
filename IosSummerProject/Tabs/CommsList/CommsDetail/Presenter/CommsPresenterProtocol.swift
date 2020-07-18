c9c4d6aa82445a9f83eb37c06d8652e46285edd0
akashmair <akash_mair@hotmail.co.uk>
2020-07-18 13:44:58 +0100

    fix(CommsDetail): Readded the folder structure and relative files since I couldnt select the old files

---
+++ IosSummerProject/Tabs/CommsList/CommsDetail/Presenter/CommsPresenterProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  CommsPresenterProtocol.swift
+//  IosSummerProject
+//
+//  Created by Akash Mair on 18/07/2020.
+//  Copyright © 2020 Liiban Mukhtar. All rights reserved.
+//
+
+import Foundation
