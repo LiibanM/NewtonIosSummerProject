124c56a344ffeb9ac5e329d22c29874aca1fadb6
akashmair <akash_mair@hotmail.co.uk>
2020-07-20 11:35:30 +0100

    Created CommsCell .xib added imageview and labels

---
+++ IosSummerProject/Tabs/CommsList/View/CommsCell/CommsCell.swift
@@ -0,0 +1,25 @@
+//
+//  CommsCell.swift
+//  IosSummerProject
+//
+//  Created by Akash Mair on 20/07/2020.
+//  Copyright © 2020 Liiban Mukhtar. All rights reserved.
+//
+
+import UIKit
+
+class CommsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
