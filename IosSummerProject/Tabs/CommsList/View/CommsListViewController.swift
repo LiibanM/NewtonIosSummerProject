0d7153f54e2db32f69f46df13b24689387740c03
Sheikh <sheikh.momin.ahmad@and.digital>
2020-07-20 15:11:47 +0100

    feat

--- IosSummerProject/Tabs/CommsList/View/CommsListViewController.swift
+++ IosSummerProject/Tabs/CommsList/View/CommsListViewController.swift
@@ -13,12 +13,14 @@ class CommsListViewController: UIViewController, Storyboarded {
     @IBOutlet weak var commsListTableView: UITableView!
     var commsListPresenter: CommsListPresenterProtocol!
     
-    var comms = [Any]()
+    var comms = ["Hello"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         commsListTableView.dataSource = self
+        
+        commsListTableView.delegate = self
         
         commsListTableView.register(UINib.init(nibName: "CommsCell", bundle: nil), forCellReuseIdentifier:"CommsCell" )
 
@@ -55,12 +57,17 @@ extension CommsListViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CommsCell", for: indexPath) as! CommsCell
         
         cell.commsTitleLabel.text = "Kash Money"
+        cell.commsCategoryLabel.text = "Awesome"
         
         
         return cell
         
     }
-    
-    
+}
+
+extension CommsListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("hello")
+    }
 }
 
