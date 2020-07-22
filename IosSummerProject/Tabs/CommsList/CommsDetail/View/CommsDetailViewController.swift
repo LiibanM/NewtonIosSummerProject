56270670451fbe624098a36bb19b898b3d6715fa
Bradley Burgess <bradley.burgess@and.digital>
2020-07-22 15:38:58 +0100

    feat: added support for external images, added edit menu button

--- IosSummerProject/Tabs/CommsList/CommsDetail/View/CommsDetailViewController.swift
+++ IosSummerProject/Tabs/CommsList/CommsDetail/View/CommsDetailViewController.swift
@@ -10,14 +10,7 @@ import UIKit
 
     
 class CommsDetailViewController: UIViewController, Storyboarded {
-    
-    let dummyData: [String: Any] = [
-        "id": 123456,
-        "title": "This is a title.",
-        "tag": "covid-19",
-        "description": "This is a description"
-    ]
-    
+        
     @IBOutlet weak var commsScrollView: UIScrollView!
     @IBOutlet weak var commsImageView: UIImageView!
     @IBOutlet weak var commsLabelView: UILabel!
@@ -29,26 +22,68 @@ class CommsDetailViewController: UIViewController, Storyboarded {
         commsScrollView.refreshControl?.addTarget(self, action:
                                            #selector(handleRefreshControl),
                                            for: .valueChanged)
+        
+        // this is dummy data (will eventually be from backend)
+        
+        let image = "https://www.lifeline-security.co.uk/wp-content/uploads/2020/03/covid-19.png"
+        let title = "CRT Comms"
+        let description = "Hi Everyone, Please see the following slides detailing changes to the existing policies and processes which we have put in place to help us manage the business in the current climate we are operating in.\n\nThe slides confirm and clarify the changes mentioned by Paramjit in the last all AND call.They cover the following 3 key areas:\n\n\u{2022} Holiday Policy changes\n\u{2022} Probation Period changes\n\u{2022} Performance & Progression changes\n\nYou will be receiving some additional comms from your LoB (Line of Business) Leads explaining what follow up actions will be either required by you or put in place as a result of the changes that have been made.\n\nThank you to everyone who completed the last Illume survey. Results are being analysed so we can fully understand your view and take appropriate action to continue making sure we’re looking after our ANDis. From just over 500 responses, 93% of you are feeling confident about the company and 89% are feeling positive about the way we are communicating. A big thank you for your confidence in our company and the way we are dealing with this crisis."
+        
+        self.commsImageView.downloaded(from: image)
+        self.navigationItem.title = title
+        self.commsLabelView.text = title
+        self.commsDescriptionView.text = description
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(editButtonTapped))
 
     }
     
     @objc func handleRefreshControl() {
-       // Update your content…
-
+       // Fetch the comm data from database & update elements
        // Dismiss the refresh control.
        DispatchQueue.main.async {
           self.commsScrollView.refreshControl?.endRefreshing()
        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func editButtonTapped() {
+           // open screen to edit comm
+       }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
-    */
 
+    func downloadImage(from url: URL) {
+        print("Download Started")
+        getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { [weak self] in
+                self?.commsImageView.image = UIImage(data: data)
+            }
+        }
+    }
+}
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
 }
