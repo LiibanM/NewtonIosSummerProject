7d72a8f1931a15881fce2d1a11323af10780cdd4
Akash-Mair <akash_mair@hotmailco.uk>
2020-07-22 21:00:23 +0100

    test(CommsList): added method to mockPresenter

--- IosSummerProjectTests/Tabs/CommsList/Mocks/MockCommsListPresenter.swift
+++ IosSummerProjectTests/Tabs/CommsList/Mocks/MockCommsListPresenter.swift
@@ -10,6 +10,10 @@ import Foundation
 @testable import IosSummerProject
 
 class MockCommsListPresenter: CommsListPresenterProtocol {
+    func didTapComm(with id: Int) {
+        
+    }
+    
     var didTapAddCommsCount = 0
     
     
@@ -18,14 +22,14 @@ class MockCommsListPresenter: CommsListPresenterProtocol {
     }
     
     func loadData() {
-        let articles = [
-                   Article(title: "First post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceNow: 86400), highlighted: true, image: "https://picsum.photos/200"),
-                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 67893), highlighted: false, image: "https://picsum.photos/200"),
-                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 67893), highlighted: true, image: "https://picsum.photos/200"),
-                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 86400), highlighted: false, image: "https://picsum.photos/200"),
-                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 67833), highlighted: true, image: "https://picsum.photos/200"),
-                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 90000), highlighted: false, image: "https://picsum.photos/200"),
-                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(), highlighted: false, image: "https://picsum.photos/200")
-               ]               
+//        let articles = [
+//                   Article(title: "First post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceNow: 86400), highlighted: true, image: "https://picsum.photos/200"),
+//                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 67893), highlighted: false, image: "https://picsum.photos/200"),
+//                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 67893), highlighted: true, image: "https://picsum.photos/200"),
+//                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 86400), highlighted: false, image: "https://picsum.photos/200"),
+//                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 67833), highlighted: true, image: "https://picsum.photos/200"),
+//                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(timeIntervalSinceReferenceDate: 90000), highlighted: false, image: "https://picsum.photos/200"),
+//                   Article(title: "Second post", content: "This is content", category: Category(category_id: 1, category_name: "COVID-19") , date: Date(), highlighted: false, image: "https://picsum.photos/200")
+//               ]
     }
 }
