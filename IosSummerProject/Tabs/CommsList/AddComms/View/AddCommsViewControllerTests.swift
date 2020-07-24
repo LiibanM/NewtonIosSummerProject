a7d0a1318d74b7161c22fea5a33bce62aa053806
nikola_drangovski <nikola.drangovski@and.digital>
2020-07-24 09:25:40 +0100

    fix(addComms):comment tests

--- IosSummerProject/Tabs/CommsList/AddComms/View/AddCommsViewControllerTests.swift
+++ IosSummerProject/Tabs/CommsList/AddComms/View/AddCommsViewControllerTests.swift
@@ -22,21 +22,21 @@ class AddCommsViewControllerTests: XCTestCase {
         addCommsViewController = nil
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-    func testUploadImageButtonTapped(){
-        
-    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+//
+//    func testUploadImageButtonTapped(){
+//
+//    }
 
 }
