35eea207e767358b7c5cc0f0fe1776fed56b122e
alexmarchant-anddigital <alexander.marchant@and.digital>
2020-07-29 23:13:19 +0100

    Add Category comments

--- IosSummerProject/Services/Models/Category.swift
+++ IosSummerProject/Services/Models/Category.swift
@@ -9,6 +9,14 @@
 import Foundation
 
 struct Category: Codable {
+    
+    // Swift variables are camelCase
+    // Spaces do not use an _
+    // You use an uppercase letter where a space is
+    
+    // let categoryId: Int
+    // let categoryName: String
+    
     let category_id: Int
     let category_name: String
 }
